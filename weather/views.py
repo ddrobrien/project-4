9d567ea9cc1023badbd2250dbc5fd8b8973bc0de
David O'Brien <ddrobrien@icloud.com>
2023-01-18 10:39:26 +0000

    templates and urls linked up and working displaying first city weather data*london

--- weather/views.py
+++ weather/views.py
@@ -1,3 +1,19 @@
+import requests
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=4f265cde61bea7c1241d1df736c486dd'
+    city = 'london'
+
+    r = requests.get(url.format(city)).json()
+
+    city_weather = {
+        'city': city,
+        'temperature': r['main']['temp'],
+        'description': r['weather'][0]['description'],
+        'icon': r['weather'][0]['icon'],
+    }
+
+    context = {'city_weather': city_weather}
+    return render(request, 'weather/weather.html', context)
